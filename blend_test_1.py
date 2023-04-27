0f70e2269c035ad342013901739e07babcf44fe9
mprib <31831778+mprib@users.noreply.github.com>
2023-04-26 20:36:39 -0500

    end of day 2 of donut

--- blend_test_1.py
+++ blend_test_1.py
@@ -4,6 +4,7 @@ C = bpy.context
 
 print(C.object)
 
-for i in range(0,4):
-    bpy.ops.mesh.primitive_monkey_add(enter_editmode=False, align='WORLD', location=(0, i, i*2), scale=(1, 1, 1))
-
+for i in range(0, 4):
+    bpy.ops.mesh.primitive_monkey_add(
+        enter_editmode=False, align="WORLD", location=(0, i, i * 2), scale=(1, 1, 1)
+    )
