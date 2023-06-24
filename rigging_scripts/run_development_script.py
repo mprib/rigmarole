8a21a4c05a0bed6176ee6632edafb91e3bfdd030
mprib <prible@gmail.com>
2023-06-24 18:36:08 -0500

    convenience script..learn how to run with keyboard

---
+++ rigging_scripts/run_development_script.py
@@ -0,0 +1,13 @@
+# This stub runs a python script relative to the currently open
+# blend file, useful when editing scripts externally.
+
+import bpy
+import os
+
+# Use your own script name here:
+filename = "C:\Users\Mac Prible\repos\learn_blender\rigging_scripts\test_meta_ik_anim.py"
+
+filepath = os.path.join(os.path.dirname(bpy.data.filepath), filename)
+global_namespace = {"__file__": filepath, "__name__": "__main__"}
+with open(filepath, 'rb') as file:
+    exec(compile(file.read(), filepath, 'exec'), global_namespace)
