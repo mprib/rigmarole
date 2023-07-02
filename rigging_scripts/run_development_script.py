effec936e95ffd6a9f1f55e79ad55452dc187ea8
mprib <prible@gmail.com>
2023-07-02 12:02:32 -0500

    wrapping up in functions

--- rigging_scripts/run_development_script.py
+++ rigging_scripts/run_development_script.py
@@ -5,10 +5,19 @@ import bpy
 import os
 
 # Use your own script name here:
-# filename = r"C:\Users\Mac Prible\repos\learn_blender\rigging_scripts\test_meta_ik_anim.py"
-filename = r"C:\Users\Mac Prible\repos\learn_blender\rigging_scripts\import_empties.py"
 
-filepath = os.path.join(os.path.dirname(bpy.data.filepath), filename)
-global_namespace = {"__file__": filepath, "__name__": "__main__"}
-with open(filepath, 'rb') as file:
-    exec(compile(file.read(), filepath, 'exec'), global_namespace)
+
+
+def run_script(filename):
+    filepath = os.path.join(os.path.dirname(bpy.data.filepath), filename)
+    global_namespace = {"__file__": filepath, "__name__": "__main__"}
+    with open(filepath, 'rb') as file:
+        exec(compile(file.read(), filepath, 'exec'), global_namespace)
+
+
+filename = r"C:\Users\Mac Prible\repos\learn_blender\rigging_scripts\import_empties.py"
+run_script(filename)
+
+
+#filename = r"C:\Users\Mac Prible\repos\learn_blender\rigging_scripts\load_meta_ik_anim.py"
+#run_script(filename)
