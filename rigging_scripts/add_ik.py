af02ef279e443e13a2097e180532a85635e0c450
mprib <prible@gmail.com>
2023-07-06 07:35:00 -0500

    beginning to create seperate IK script

---
+++ rigging_scripts/add_ik.py
@@ -0,0 +1,12 @@
+targets_IK = {
+    
+"lid.T.L.003":"left_inner_eye",
+"shoulder.R":"right_shoulder",
+"upper_arm.R":"right_elbow",
+"forearm.R":"right_wrist",
+""
+
+"lid.T.R.003":"right_inner_eye",
+
+    
+}
