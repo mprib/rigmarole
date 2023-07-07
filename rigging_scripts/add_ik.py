59b9b1029224fe032d65a85b2b747911ef2f3fd6
mprib <prible@gmail.com>
2023-07-07 09:16:50 -0500

    build out bone target

--- rigging_scripts/add_ik.py
+++ rigging_scripts/add_ik.py
@@ -8,7 +8,7 @@ bpy.ops.object.mode_set(mode='POSE')
 rig = bpy.data.objects['human_rig']
 
 # dict[bone:empty_target]
-targets_IK = {
+bone_targets = {
     "lid.T.R.003":"right_inner_eye",
     "lid.T.L.003":"left_inner_eye",
     "shoulder.R":"right_shoulder",
@@ -22,10 +22,31 @@ targets_IK = {
     "shin.R":"right_ankle",
     "shin.L":"left_ankle",
     "foot.R":"right_foot_index",
-    "foot.L":"left_foot_index"
+    "foot.L":"left_foot_index",
+    "f_pinky.03.R":"right_pinky_tip",
+    "f_pinky.03.L":"left_pinky_tip",
+    "f_ring.03.R":"right_ring_finger_tip",
+    "f_ring.03.L":"left_ring_finger_tip",
+    "f_index.03.L":"left_index_finger_tip",
+    "f_index.03.R":"right_index_finger_tip",
+    "f_middle.03.L":"left_middle_finger_tip",
+    "f_middle.03.R":"right_middle_finger_tip",
+    "thumb.03.L":"left_thumb_tip",
+    "thumb.03.R":"right_thumb_tip",
 }
 
-for bone_name, target_name in targets_IK.items():
+# by default, chain counts will be set to 1, if something else is wanted configure it here
+chain_counts = {
+    "lid.T.R.003":0,
+    "lid.T.L.003":0,
+    "f_pinky.03.R":5,
+    "f_pinky.03.L":5,
+    "f_ring.03.R":5,
+    "f_ring.03.L":5,
+}
+
+
+for bone_name, target_name in bone_targets.items():
     bone = rig.pose.bones[bone_name]
 
     # Add the IK constraint to the bone
@@ -35,4 +56,8 @@ for bone_name, target_name in targets_IK.items():
     ik_constraint.target = bpy.data.objects[target_name]
     
     # Set the chain count
-    ik_constraint.chain_count = 1
+    if bone_name in chain_counts.keys():
+        ik_constraint.chain_count = chain_counts[bone_name]
+    else:
+        ik_constraint.chain_count = 1
+
